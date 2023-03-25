117102ab47fd7869cbb764493ad9b70e9b92cd38
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-03-24 21:45:03 -0400

    hardcode of the Meal Tracker

--- caloriechecker/src/Components/Diary.js
+++ caloriechecker/src/Components/Diary.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Title from '../Modules/Title';
 import FoodCalendar from '../Modules/Diary/Calendar';
+import MealTable from '../Modules/Diary/Mealtable';
 
 
 function Diary() {
   return (
     <div>
-      <Title />
-      <FoodCalendar />
-     </div>
+      <div>
+        <Title />
+        <FoodCalendar />
+      </div>
+      <div>
+        <h1>My Meal Tracker</h1>
+        <MealTable />
+      </div>
+    </div>
   );
 }
 
