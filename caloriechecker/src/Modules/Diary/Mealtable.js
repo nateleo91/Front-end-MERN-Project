d3b5305413dfee90ccda691b54d6c6287445cbd1
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-03-25 13:21:26 -0400

    Add form elements for adding a new meal item to the table

--- caloriechecker/src/Modules/Diary/Mealtable.js
+++ caloriechecker/src/Modules/Diary/Mealtable.js
@@ -44,4 +44,20 @@ const MealTable = () => {
           <option value="dinner">Dinner</option>
           <option value="snacks">Snacks</option>
         </select>
-       
+        <label htmlFor="foodItem">Food Item:</label>
+        <input id="foodItem" name="foodItem" type="text" value={foodItem} onChange={(event) => setFoodItem(event.target.value)} />
+        <label htmlFor="calories">Calories:</label>
+        <input id="calories" name="calories" type="number" value={calories} onChange={(event) => setCalories(event.target.value)} />
+        <label htmlFor="carbs">Carbs:</label>
+        <input id="carbs" name="carbs" type="number" value={carbs} onChange={(event) => setCarbs(event.target.value)} />
+        <label htmlFor="fat">Fat:</label>
+        <input id="fat" name="fat" type="number" value={fat} onChange={(event) => setFat(event.target.value)} />
+        <label htmlFor="protein">Protein:</label>
+        <input id="protein" name="protein" type="number" value={protein} onChange={(event) => setProtein(event.target.value)} />
+        <label htmlFor="sodium">Sodium:</label>
+        <input id="sodium" name="sodium" type="number" value={sodium} onChange={(event) => setSodium(event.target.value)} />
+        <label htmlFor="sugar">Sugar:</label>
+        <input id="sugar" name="sugar" type="number" value={sugar} onChange={(event) => setSugar(event.target.value)} />
+        <button type="submit">Add Food</button>
+      </form>
+      
