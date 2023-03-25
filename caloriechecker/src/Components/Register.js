6f5ec021d24cf67d1d0519b52f074acc8ed2c655
Jose Padilla <jose.apadilla99@gmail.com>
2023-03-24 21:27:17 -0500

    set up register component

---
+++ caloriechecker/src/Components/Register.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Title from "../Modules/Title";
+import RegisterForm from "../Modules/Login&register/RegisterForm";
+
+function Register() {
+    <div>
+        <Title />
+        <RegisterForm />
+    </div>
+
+}
+
+export default Register
---
+++ caloriechecker/src/Modules/Login&register/RegisterForm.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function RegisterForm() {
+
+    
+    return(
+        <div>
+        <h2>Sign Up</h2>
+
+        <form>
+          <div>
+            <label htmlFor='email'>Email</label>
+            <input type='text' name='email' onChange={this.props.handleInput} />
+          </div>
+
+          <div>
+            <label htmlFor='password'>Password</label>
+            <input type='text' name='password' onChange={this.props.handleInput} />
+          </div>
+          <input value='Submit' type='submit' onClick={this.props.handleSignUp} />
+        </form>
+      </div>
+    )
+}
+
+export default RegisterForm
