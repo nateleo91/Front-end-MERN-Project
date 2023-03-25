5b2f85699d007a3993ea7430c05fb4be73dfdd28
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-03-25 13:07:27 -0400

    Added state variables and initial state for the MealTable component.

--- caloriechecker/src/Modules/Diary/Mealtable.js
+++ caloriechecker/src/Modules/Diary/Mealtable.js
@@ -1,111 +1,13 @@
 import React, { useState } from "react";
-import { Table, Button, Form } from "react-bootstrap";
 
 const MealTable = () => {
-  const [breakfast, setBreakfast] = useState([]);
-  const [lunch, setLunch] = useState([]);
-  const [dinner, setDinner] = useState([]);
-  const [newFood, setNewFood] = useState("");
+  const [meal, setMeal] = useState("breakfast");
+  const [foodItem, setFoodItem] = useState("");
+  const [calories, setCalories] = useState("");
+  const [carbs, setCarbs] = useState("");
+  const [fat, setFat] = useState("");
+  const [protein, setProtein] = useState("");
+  const [sodium, setSodium] = useState("");
+  const [sugar, setSugar] = useState("");
+  const [mealItems, setMealItems] = useState([]);
 
-  const handleAddFood = (meal) => {
-    if (newFood.trim() === "") return;
-    switch (meal) {
-      case "breakfast":
-        setBreakfast([...breakfast, newFood]);
-        break;
-      case "lunch":
-        setLunch([...lunch, newFood]);
-        break;
-      case "dinner":
-        setDinner([...dinner, newFood]);
-        break;
-      default:
-        break;
-    }
-    setNewFood("");
-  };
-
-  return (
-    <div>
-      <h2>My Meal Table</h2>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Meal</th>
-            <th>Food</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Breakfast</td>
-            <td>
-              {breakfast.map((food, index) => (
-                <div key={index}>{food}</div>
-              ))}
-              <Form>
-                <Form.Control
-                  type="text"
-                  placeholder="Add food"
-                  value={newFood}
-                  onChange={(e) => setNewFood(e.target.value)}
-                />
-              </Form>
-              <Button
-                variant="primary"
-                onClick={() => handleAddFood("breakfast")}
-              >
-                Add
-              </Button>
-            </td>
-          </tr>
-          <tr>
-            <td>Lunch</td>
-            <td>
-              {lunch.map((food, index) => (
-                <div key={index}>{food}</div>
-              ))}
-              <Form>
-                <Form.Control
-                  type="text"
-                  placeholder="Add food"
-                  value={newFood}
-                  onChange={(e) => setNewFood(e.target.value)}
-                />
-              </Form>
-              <Button
-                variant="primary"
-                onClick={() => handleAddFood("lunch")}
-              >
-                Add
-              </Button>
-            </td>
-          </tr>
-          <tr>
-            <td>Dinner</td>
-            <td>
-              {dinner.map((food, index) => (
-                <div key={index}>{food}</div>
-              ))}
-              <Form>
-                <Form.Control
-                  type="text"
-                  placeholder="Add food"
-                  value={newFood}
-                  onChange={(e) => setNewFood(e.target.value)}
-                />
-              </Form>
-              <Button
-                variant="primary"
-                onClick={() => handleAddFood("dinner")}
-              >
-                Add
-              </Button>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
-  );
-};
-
-export default MealTable;
