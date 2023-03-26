13f7bf1aac23e14e9509852ddc462bb1c1a7b7b7
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-03-26 16:26:16 -0400

    Initializing the state of MealTable component

--- caloriechecker/src/Modules/Diary/Mealtable.js
+++ caloriechecker/src/Modules/Diary/Mealtable.js
@@ -1,109 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
+import "../../css/Mealtable.css";
 
-const MealTable = () => {
-  const [meal, setMeal] = useState("breakfast");
-  const [foodItem, setFoodItem] = useState("");
-  const [calories, setCalories] = useState("");
-  const [carbs, setCarbs] = useState("");
-  const [fat, setFat] = useState("");
-  const [protein, setProtein] = useState("");
-  const [sodium, setSodium] = useState("");
-  const [sugar, setSugar] = useState("");
-  const [mealItems, setMealItems] = useState([]);
-  const [foodData, setFoodData] = useState(null);
+class MealTable extends React.Component {
+  constructor() {
+    super();
 
-
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch(
-        "https://api.edamam.com/api/food-database/v2/parser?nutrition-type=logging&ingr=chicken%20breast&app_id=YOUR_APP_ID&app_key=YOUR_APP_KEY"
-      );
-      const data = await result.json();
-      setFoodData(data);
-    };
-    fetchData();
-  }, []);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const newMealItem = {
-      meal: meal,
-      foodItem: foodItem,
-      calories: calories,
-      carbs: carbs,
-      fat: fat,
-      protein: protein,
-      sodium: sodium,
-      sugar: sugar,
-    };
-    setMealItems([...mealItems, newMealItem]);
-    setFoodItem("");
-    setCalories("");
-    setCarbs("");
-    setFat("");
-    setProtein("");
-    setSodium("");
-    setSugar("");
-  };
-
-  return (
-    <div>
-      <h1>Meal Table</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="meal">Meal:</label>
-        <select id="meal" name="meal" value={meal} onChange={(event) => setMeal(event.target.value)}>
-          <option value="breakfast">Breakfast</option>
-          <option value="lunch">Lunch</option>
-          <option value="dinner">Dinner</option>
-          <option value="snacks">Snacks</option>
-        </select>
-        <label htmlFor="foodItem">Food Item:</label>
-        <input id="foodItem" name="foodItem" type="text" value={foodItem} onChange={(event) => setFoodItem(event.target.value)} />
-        <label htmlFor="calories">Calories:</label>
-        <input id="calories" name="calories" type="number" value={calories} onChange={(event) => setCalories(event.target.value)} />
-        <label htmlFor="carbs">Carbs:</label>
-        <input id="carbs" name="carbs" type="number" value={carbs} onChange={(event) => setCarbs(event.target.value)} />
-        <label htmlFor="fat">Fat:</label>
-        <input id="fat" name="fat" type="number" value={fat} onChange={(event) => setFat(event.target.value)} />
-        <label htmlFor="protein">Protein:</label>
-        <input id="protein" name="protein" type="number" value={protein} onChange={(event) => setProtein(event.target.value)} />
-        <label htmlFor="sodium">Sodium:</label>
-        <input id="sodium" name="sodium" type="number" value={sodium} onChange={(event) => setSodium(event.target.value)} />
-        <label htmlFor="sugar">Sugar:</label>
-        <input id="sugar" name="sugar" type="number" value={sugar} onChange={(event) => setSugar(event.target.value)} />
-        <button type="submit">Add Food</button>
-      </form>
-      <table>
-        <thead>
-          <tr>
-            <th>Meal</th>
-            <th>Food Item</th>
-            <th>Calories</th>
-            <th>Carbs</th>
-            <th>Fat</th>
-            <th>Protein</th>
-            <th>Sodium</th>
-            <th>Sugar</th>
-          </tr>
-        </thead>
-        <tbody>
-          {mealItems.map((mealItem, index) => (
-            <tr key={index}>
-              <td>{mealItem.meal}</td>
-              <td>{mealItem.foodItem}</td>
-              <td>{mealItem.calories}</td>
-              <td>{mealItem.carbs}</td>
-              <td>{mealItem.fat}</td>
-              <td>{mealItem.protein}</td>
-              <td>{mealItem.sodium}</td>
-              <td>{mealItem.sugar}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-export default MealTable;
+    this.state = {
+      meals: [
+        {
+          mealType: 'Breakfast',
+          calories: 0,
+          carbs: 0,
+          fat: 0,
+          protein: 0,
+          sodium: 0,
+          sugar: 0
+        },
