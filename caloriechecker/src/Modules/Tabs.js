40a152624bee5abe59d0a3dab38243b4c7243019
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-03-26 12:50:41 -0400

    Added a sign in form to the modal with email and password fields and a Sign In button

--- caloriechecker/src/Modules/Tabs.js
+++ caloriechecker/src/Modules/Tabs.js
@@ -45,4 +45,25 @@ function Tabs() {
         </Button>
       </div>
 
-   
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Weightloss</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" onClick={handleSignInSubmit}>
+              Sign In
+            </Button>
+          </Form>
+
+          <hr />
