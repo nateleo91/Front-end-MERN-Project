fdd8eab97518b63e69000c5d834e64de84c10bab
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-03-26 12:44:40 -0400

    Add form submission handlers for sign-in and sign-up modals in Tabs component

--- caloriechecker/src/Modules/Tabs.js
+++ caloriechecker/src/Modules/Tabs.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Form } from 'react-bootstrap';
 import '../css/tabs.css'
 
 function Tabs() {
-    const navigate = useNavigate()
-    const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
 
   const handleShowModal = () => {
     setShowModal(true);
@@ -15,38 +15,14 @@ function Tabs() {
     setShowModal(false);
   };
 
-return (    
-  <div className="buttons">
-    <div className="button-group">
-      <button className="button" onClick={() => navigate('/diary')}>
-        Diary
-      </button>
-        |
-      <button className="button" onClick={() => navigate('/create')}>
-        Create
-      </button>
-        |
-      <button className="button" onClick={() => navigate('/AboutUs')}>
-        AboutUs
-      </button>
+  const handleSignInSubmit = (event) => {
+    event.preventDefault();
+    // handle sign-in form submission here
+  };
 
-      <Button className='sign' variant="primary" onClick={handleShowModal}>
-        SignIn/Sign-up
-      </Button>
-    </div>
+  const handleSignUpSubmit = (event) => {
+    event.preventDefault();
+    // handle sign-up form submission here
+  };
 
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Popup Title</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Popup Content</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleCloseModal}>
-              Close
-            </Button>
-          </Modal.Footer>
-      </Modal>
-  </div>
-)}
-
-export default Tabs
+ 
