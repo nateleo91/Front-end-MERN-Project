85c15294467d3fb302ef555374dcd0896e462edf
Jose Padilla <jose.apadilla99@gmail.com>
2023-03-26 17:17:30 -0500

    bug: can't tell if user is logging in

--- caloriechecker/src/app.js
+++ caloriechecker/src/app.js
@@ -1,54 +1,56 @@
-import Home from "./Components/Home";
-import Diary from "./Components/Diary";
-import Create from "./Components/Create";
-import AboutUs from "./Components/AboutUs";
-import Register from "./Components/Register";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
-import axios from "axios";
+import Home from './Components/Home'
+import Diary from './Components/Diary'
+import Create from './Components/Create'
+import AboutUs from './Components/AboutUs'
+import Register from './Components/Register'
+import Login from './Components/Login'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
+
   const [user, setUser] = useState({
-    email: "",
-    password: "",
-    isLoggedIn: false,
-  });
+    email: '',
+    password: '',
+    isLoggedIn: false
+  })
 
   function handleInput(e) {
     setUser({
-      // spread operator needed to save input of previous state
+      // spread operator needed to save input of previous state 
       ...user,
-      [e.target.name]: e.target.value,
-    });
+      [e.target.name]: e.target.value
+    })
   }
   async function handleSignUp(e) {
-    e.preventDefault();
-    await axios
-      .post("http://localhost:4000/users/signup", {
-        //Change url on deployment
-        email: user.email,
-        password: user.password,
-      })
-      .then((res) => {
-        localStorage.token = res.data.token;
-        setUser({ isLoggedIn: true });
-      })
-      .catch((error) => console.log(error));
+    e.preventDefault()
+    await axios.post("http://localhost:4000/users/signup", {
+      //Change url on deployment
+      email: user.email,
+      password: user.password
+    })
+    .then( res => {
+      localStorage.token = res.data.token
+      setUser({ isLoggedIn: true })
+    })
+    .catch(error => console.log(error))
   }
 
   async function handleLogIn(e) {
-    e.preventDefault();
-    axios
-      .post("http://localhost:4000/users/login", {
-        //change url on deployment
-        email: user.email,
-        password: user.password,
-      })
-      .then((res) => {
-        localStorage.token = res.data.token;
-        setUser({ isLoggedIn: true });
-      })
-      .catch((error) => console.log(error));
+    console.log(user.email, user.password)
+    e.preventDefault()
+    axios.post("http://localhost:4000/users/login",{
+      //change url on deployment
+      email: user.email,
+      password: user.password
+    })
+    .then(res => {
+      localStorage.token = res.data.token
+      setUser({ isLoggedIn: true })
+      console.log("logged in")
+    })
+    .catch(error => console.log(error))
   }
 
   return (
@@ -58,21 +60,11 @@ function App() {
         <Route path="/diary" element={<Diary />} />
         <Route path="/Create" element={<Create />} />
         <Route path="/AboutUs" element={<AboutUs />} />
-        <Route
-          path="/Register"
-          element={
-            <Register handleInput={handleInput} handleSignUp={handleSignUp} />
-          }
-        />
-        <Route
-          path="/Login"
-          element={
-            <Login handleInput={handleInput} handleLogin={handleLogIn} />
-          }
-        />
+        <Route path="/Register" element={<Register handleInput={handleInput} handleSignUp={handleSignUp}/>} />
+        <Route path="/Login" element={<Login handleInput={handleInput} handleLogin={handleLogIn} />} />
       </Routes>
     </Router>
   );
 }
 
-export default App;
+export default App
