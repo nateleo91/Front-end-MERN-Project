570a7de8d5e3759ba220f3581d7a24a8007c1e57
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-03-25 21:33:40 -0400

    Added a date picker to the FoodCalendar component with min and max dates and updated the display to show the selected date.

--- caloriechecker/src/Modules/Diary/Calendar.js
+++ caloriechecker/src/Modules/Diary/Calendar.js
@@ -16,4 +16,24 @@ function FoodCalendar() {
     day: "numeric",
   });
 
-  
+  return (
+    <div>
+      <h1>Your Food Diary For: {dateString}</h1>
+      <div>
+        <label htmlFor="date" style={{ display: "inline-block" }}>
+          Date:{" "}
+        </label>
+        <DatePicker
+          id="date"
+          selected={selectedDate}
+          onChange={handleDateChange}
+          dateFormat="yyyy-MM-dd"
+          minDate={new Date("1900-01-01")}
+          maxDate={new Date("2100-12-31")}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default FoodCalendar;
