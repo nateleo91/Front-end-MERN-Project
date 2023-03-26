212d88465d0f6db59becd193db23c298a9cdf9c5
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-03-26 12:58:10 -0400

    Added sign-up form to the modal for user registration

--- caloriechecker/src/Modules/Tabs.js
+++ caloriechecker/src/Modules/Tabs.js
@@ -67,3 +67,36 @@ function Tabs() {
           </Form>
 
           <hr />
+
+          <Form>
+            <Form.Group controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter name" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" onClick={handleSignUpSubmit}>
+              Sign Up
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+export default Tabs;
