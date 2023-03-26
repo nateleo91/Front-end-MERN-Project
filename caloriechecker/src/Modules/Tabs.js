bb2aa9e75fb671656c27d928239e97e023bdaa7f
natleo91 <leonard91j@gmail.com>
2023-03-26 02:17:57 +0000

    sign-in button, rough copy

--- caloriechecker/src/Modules/Tabs.js
+++ caloriechecker/src/Modules/Tabs.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Modal, Button } from 'react-bootstrap';
 import '../css/tabs.css'
 
 function Tabs() {
     const navigate = useNavigate()
+    const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
 
 return (    
 <div className="buttons">
@@ -18,6 +28,22 @@ return (
             <button class="button" onClick={() => navigate('/AboutUs')}>
               AboutUs
             </button>
+
+            <Button class='sign' variant="primary" onClick={handleShowModal}>
+            SignIn/Sign-up
+            </Button>
+
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Popup Title</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Popup Content</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleCloseModal}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
       </div>
 )
 }
