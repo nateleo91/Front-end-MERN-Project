c3178fac66eeccdd911558baeb95d51e48332832
Jonny Grobstein <jonnygrobstein@gmail.com>
2023-04-01 11:10:21 -0500

    quick add to get back in the swing

--- caloriechecker/src/Modules/Create/NutritionInput.js
+++ caloriechecker/src/Modules/Create/NutritionInput.js
@@ -43,36 +43,36 @@ function NutritionInput() {
 		<h2>Enter Nutritional Information</h2>
 
 		<form className="createForm" onSubmit={handleSubmit}>
-    <label htmlFor="name-input">Food name:</label>
-    <input type="text" id="name-input" name="food_name" value={food_name} onChange={handleInputChange} />
-	<label htmlFor="calories-input">Calories:</label>
-<input type="number" className="numInput" id="calories-input" name="calories" value={calories} onChange={handleInputChange} step="1" />
-<label id="createLabel" htmlFor="total-fat-input">Total Fat:</label>
-<input type="number" className="numInput" id="total-fat-input" name="totalFat" value={totalFat} onChange={handleInputChange} step="1" />
-<label id="createLabel" htmlFor="saturated-input">Saturated Fat:</label>
-<input type="number" className="numInput" id="saturated-input" name="saturatedFat" value={saturatedFat} onChange={handleInputChange} step="1" />
-<label id="createLabel" htmlFor="polyunsaturated-input">Polyunsaturated Fat:</label>
-<input type="number" className="numInput" id="polyunsaturated-input" name="polyunsaturatedFat" value={polyunsaturatedFat} onChange={handleInputChange} step="1" />
-<label id="createLabel" htmlFor="monounsaturated-input">Monounsaturated Fat:</label>
-<input type="number" className="numInput" id="monounsaturated-input" name="monounsaturatedFat" value={monounsaturatedFat} onChange={handleInputChange} step="1" />
-<label id="createLabel" htmlFor="trans-input">Trans Fat:</label>
-<input type="number" className="numInput" id="trans-input" name="transFat" value={transFat} onChange={handleInputChange} step="1" />
-<label id="createLabel" htmlFor="cholesterol-input">Cholesterol:</label>
-<input type="number" className="numInput" id="cholesterol-input" name="cholesterol" value={cholesterol} onChange={handleInputChange} step="1" />
-<label id="createLabel" htmlFor="sodium-input">Sodium:</label>
-<input type="number" className="numInput" id="sodium-input" name="sodium" value={sodium} onChange={handleInputChange} step="1" />
-<label id="createLabel" htmlFor="total-carbs-input">Total Carbohydrates:</label>
-<input type="number" className="numInput" id="total-carbs-input" name="carbohydrate" value={carbohydrate} onChange={handleInputChange} step="1" />
-<label id="createLabel" htmlFor="dietary-fiber-input">Dietary Fiber:</label>
-<input type="number" className="numInput" id="dietary-fiber-input" name="fiber" value={fiber} onChange={handleInputChange} step="1" />
-<label id="createLabel" htmlFor="sugars-input">Sugars:</label>
-<input type="number" className="numInput" id="sugars-input" name="sugar" value={sugar} onChange={handleInputChange} step="1" />
-<label id="createLabel" htmlFor="protein-input">Protein:</label>
-<input type="number" className="numInput" id="protein-input" name="protein" value={protein} onChange={handleInputChange} step="1" />
-<button className="submit" type="submit">Submit</button>
-  </form>
-	  </div>
-	)
-}
+			<label htmlFor="name-input">Food name:</label>
+				<input type="text" id="name-input" name="food_name" value={food_name} onChange={handleInputChange} />
+			<label htmlFor="calories-input">Calories:</label>
+				<input type="text" className="numInput" id="calories-input" name="calories" value={calories} onChange={handleInputChange} step="1" />
+			<label id="createLabel" htmlFor="total-fat-input">Total Fat:</label>
+				<input type="text" className="numInput" id="total-fat-input" name="totalFat" value={totalFat} onChange={handleInputChange} step="1" />
+			<label id="createLabel" htmlFor="saturated-input">Saturated Fat:</label>
+				<input type="text" className="numInput" id="saturated-input" name="saturatedFat" value={saturatedFat} onChange={handleInputChange} step="1" />
+			<label id="createLabel" htmlFor="polyunsaturated-input">Polyunsaturated Fat:</label>
+				<input type="text" className="numInput" id="polyunsaturated-input" name="polyunsaturatedFat" value={polyunsaturatedFat} onChange={handleInputChange} step="1" />
+			<label id="createLabel" htmlFor="monounsaturated-input">Monounsaturated Fat:</label>
+				<input type="text" className="numInput" id="monounsaturated-input" name="monounsaturatedFat" value={monounsaturatedFat} onChange={handleInputChange} step="1" />
+			<label id="createLabel" htmlFor="trans-input">Trans Fat:</label>
+				<input type="text" className="numInput" id="trans-input" name="transFat" value={transFat} onChange={handleInputChange} step="1" />
+			<label id="createLabel" htmlFor="cholesterol-input">Cholesterol:</label>
+				<input type="text" className="numInput" id="cholesterol-input" name="cholesterol" value={cholesterol} onChange={handleInputChange} step="1" />
+			<label id="createLabel" htmlFor="sodium-input">Sodium:</label>
+				<input type="text" className="numInput" id="sodium-input" name="sodium" value={sodium} onChange={handleInputChange} step="1" />
+			<label id="createLabel" htmlFor="total-carbs-input">Total Carbohydrates:</label>
+				<input type="text" className="numInput" id="total-carbs-input" name="carbohydrate" value={carbohydrate} onChange={handleInputChange} step="1" />
+			<label id="createLabel" htmlFor="dietary-fiber-input">Dietary Fiber:</label>
+				<input type="text" className="numInput" id="dietary-fiber-input" name="fiber" value={fiber} onChange={handleInputChange} step="1" />
+			<label id="createLabel" htmlFor="sugars-input">Sugars:</label>
+				<input type="text" className="numInput" id="sugars-input" name="sugar" value={sugar} onChange={handleInputChange} step="1" />
+			<label id="createLabel" htmlFor="protein-input">Protein:</label>
+				<input type="text" className="numInput" id="protein-input" name="protein" value={protein} onChange={handleInputChange} step="1" />
+			<button className="submit" type="submit">Submit</button>
+		</form>
+			</div>
+			)
+		}
 
-export default NutritionInput
+		export default NutritionInput
