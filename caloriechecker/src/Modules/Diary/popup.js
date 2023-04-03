a37394358e5fd670af4e90c955f584fce0fd6ff1
Jonny Grobstein <jonnygrobstein@gmail.com>
2023-04-02 20:49:20 -0500

    Table almost done

--- caloriechecker/src/Modules/Diary/popup.js
+++ caloriechecker/src/Modules/Diary/popup.js
@@ -3,35 +3,28 @@ import SearchBar from '../../Modules/Home/SearchBar';
 import PopupResults from './popupResults';
 import axios from 'axios';
 
+function Popup(props) {
+  const { addFoodToMeal } = props;
+  const [searchResult, setSearchResult] = useState({});
 
+  const handleSearchQuery = async (searchText) => {
+    await axios
+      .get('https://calorie-trakr.herokuapp.com/foods/searchFood?food=' + searchText)
+      //changed url for deployment
+      .then((res) => {
+        setSearchResult(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  return (
+    <div className="popup">
+      <SearchBar handleSearchQuery={handleSearchQuery} />
+      <PopupResults searchResult={searchResult} addFoodToMeal={addFoodToMeal} />
+    </div>
+  );
+}
 
-
-
-
-
-function Popup() {
-    const [searchResult, setSearchResult] = useState({});
-  
-    const handleSearchQuery = async (searchText) => {
-
-      await axios.get("https://calorie-trakr.herokuapp.com/foods/searchFood?food=" + searchText)
-      //canged url for deployment
-        .then((res) => {
-          setSearchResult(res);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
-  
-    return (
-      <div className="popup">
-        <SearchBar handleSearchQuery={handleSearchQuery} />
-        <PopupResults searchResult={searchResult} />
-      </div>
-    );
-  }
-  
-  export default Popup;
-  
+export default Popup;
